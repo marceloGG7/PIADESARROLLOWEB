8fb89566e69bcdefa060acfa5fa39e8c654f63b7
Marcelo Araiza Molina <armamol3@gmail.com>
2023-11-16 15:34:42 -0600

    validacion de los campos del formulario de donaciones

--- js/formulario.js
+++ js/formulario.js
@@ -1,63 +1,18 @@
+const nombre = document.getElementById("nombre");
+const edad = document.getElementById("edad");
+const direccion = document.getElementById("direccion");
+const email = document.getElementById("email");
+const sexo = document.getElementById("sexo");
+const tipoSangre = document.getElementById("tipoSangre");
+const btn = document.getElementById("btn");
 
-var formulario = document.getElementsByName('Formulario')[0],
-elementos = formulario.elements,
-boton = document.getElementsById('btn');
-
-var validarNombre = function(e){
-    if (formulario.nombre.value == 0) {
-        alert("completa el campo nombre");
-        e.preventDefault();
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (
+        nombre.value.trim() === "" || edad.value.trim() === "" || direccion.value.trim() === "" ||
+        email.value.trim() === "" || sexo.value.trim() === "" || tipoSangre.value.trim() === ""
+    ) {
+        alert("Por favor, completa todos los campos.");
     }
-};
-
-var validarEdad = function(e){
-    if (formulario.edad.value == 0) {
-        alert("completa el campo edad");
-        e.preventDefault();
-    }
-};
-
-
-var validarDireccion = function(e){
-    if (formulario.direccion.value == 0) {
-        alert("completa el campo direccion");
-        e.preventDefault();
-    }
-};
-
-var validarEmail = function(e){
-    if (formulario.email.value == 0) {
-        alert("completa el campo email");
-        e.preventDefault();
-    }
-};
-
-var ValidarRadio = function(e){
-    if (formulario.sexo[0].checked == true || formulario.sexo[1],checked == true) {
-
-    } else {
-        alert("Completa el campo sexo");
-        e,preventDefault()
-    }
-};
-
-var ValidarCheckbox = function(e){
-    if (formulario.terminos.checked == false)  {
-        alert("Acepta los Terminos");
-        e,preventDefault()
-    }
-};
-
-var validar = function(e){
-    validarNombre(e);
-    validarEdad(e);
-    validarDireccion(e);
-    validarEmail(e);
-    ValidarRadio(e);
-    ValidarCheckbox(e);
-};
-
-formulario.addEventListener("submit", validar);
-
-
-
+    alert("Formulario enviado correctamente.");
+});
